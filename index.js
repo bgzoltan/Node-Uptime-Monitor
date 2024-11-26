19bddd997b3e6c4d79442562888727d30733b77b
Bagdany Zoltan <bgzoltan@gmail.com>
2024-11-26 12:59:15 +1000

    get path, method and query are added

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import http from "http";
+import url from "url";
 
-// The server responds
+// The server responds for every requests
 const server = http.createServer(function (req, res) {
-  res.end("This is my NodeJs server...");
+  // Get the url
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get method
+  const method = req.method.toLowerCase();
+
+  // Get the query string
+  const queryStringObject = parsedUrl.query;
+
+  // Logs
+  const text = `This is my NodeJs server...Path: ${path} / ${trimmedPath} Method:${method}}`;
+  console.log("Query object:", queryStringObject);
+
+  // Send a response
+  res.end(text);
 });
 
 //  Start the server
